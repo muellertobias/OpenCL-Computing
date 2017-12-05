833033625c1358c92af1720c584add2b3f066d1f
J. Kirsch <htw.jukirsch@gmail.com>
2017-12-05 12:59:36 +0100

    Aufgabe 2 angefangen

---
+++ Regularisierung/Kernel.cl
@@ -0,0 +1,11 @@
+#pragma OPENCL EXTENSION cl_khr_fp64 : enable
+
+__kernel void vecades(__global int *a, __global float *b,  __global int *c, const unsigned int n)
+{
+	int idx = get_global_id(0);
+
+	if (idx < n) 
+	{
+		c[idx] = a[idx] + b[idx] * ((-1) + 2 * (idx & 1));
+	} 
+}  
--- Regularisierung/Regularisierung.cpp
+++ Regularisierung/Regularisierung.cpp
@@ -2,7 +2,6 @@
 
 #define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 
-//#include "stdafx.h"
 #include <iostream>
 #include <CL/opencl.h>
 #include <CL/cl.h>
@@ -19,8 +18,8 @@ using namespace std;
 
 void testOpenCL(const char* kernelSource);
 char* readSourceFile(const char* filename);
-void printMatrix(const type * matrix, const size_t& n, const size_t& m);
 
+//using namespace std;
 
 int main(int argc, char* argv[])
 {
@@ -40,7 +39,9 @@ void testOpenCL(const char* kernelSource)
 {
 
 	// Device output buffer
-	cl_mem d_X;
+	cl_mem d_a;
+	cl_mem d_b;
+	cl_mem d_c;
 
 	cl_platform_id cpPlatform;		  // OpenCL platform
 	cl_device_id device_id;           // device ID
@@ -51,9 +52,14 @@ void testOpenCL(const char* kernelSource)
 
 	cl_int err;
 	// Host output array
-	type *h_matrix;
+	type *h_a;
+	type *h_b;
+	type *h_c;
+
+	size_t n = 1;
 
 	// Number of work items in each local work group
+
 	// use CL_DEVICE_MAX_WORK_ITEM_SIZES
 	cl_uint max_work_item_dimensions;
 
@@ -68,26 +74,34 @@ void testOpenCL(const char* kernelSource)
 	err = clGetDeviceIDs(cpPlatform, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
 	printf("GetDeviceIDs: %d\n", err);
 
-	// Devide Info
 	err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(max_work_item_dimensions), &max_work_item_dimensions, NULL);
 	size_t* max_work_item_sizes = (size_t*)malloc(sizeof(size_t) * max_work_item_dimensions);
 	err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_ITEM_SIZES, sizeof(size_t) * max_work_item_dimensions, max_work_item_sizes, NULL);
-	printf("\n \n");
+	
+	size_t vectorSize = 16;
 
-	size_t matrixSizeX = 16;
-	size_t matrixSizeY = 4;
+	size_t localSize = vectorSize;
+	size_t globalSize = vectorSize;
 
-	size_t localSize = matrixSizeX * matrixSizeY;
-	size_t globalSize = matrixSizeX * matrixSizeY;
+	size_t bytes = vectorSize * sizeof(type);  // Size, in bytes, of each vector
 
-	size_t bytes = globalSize * sizeof(type);  // Size, in bytes
-					 
-	h_matrix = (type*)malloc(bytes); // Allocate memory
-	memset(h_matrix, NULL, bytes);
+									  // Allocate memory for each vector on host
+	h_a = (type*)malloc(bytes);
+	h_b = (type*)malloc(bytes);
+
+	for (int i = 0;i < vectorSize;i++) {
+		h_a[i] = 1;
+		h_b[i] = 1;
+	}
+	h_c = (type*)malloc(bytes);
+	memset(h_c, NULL, bytes);
 
 	// Create a context  
 	context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
 	printf("CreateContext: %d\n", err);
+
+
+	cl_event event;
 
 	// Create a command queue 
 	queue = clCreateCommandQueueWithProperties(context, device_id, NULL, &err);
@@ -102,37 +116,47 @@ void testOpenCL(const char* kernelSource)
 	clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
 
 	// Create the compute kernel in the program we wish to run
-	kernel = clCreateKernel(program, "OpenCLID", &err);
+	kernel = clCreateKernel(program, "vecades", &err);
 	printf("CreateKernel: %d\n", err);
 
 	// Create the input and output arrays in device memory for our calculation
-	d_X = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
+	d_a = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+	d_b = clCreateBuffer(context, CL_MEM_READ_ONLY, bytes, NULL, NULL);
+	d_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, bytes, NULL, NULL);
 
-	err = clEnqueueWriteBuffer(queue, d_X, CL_TRUE, 0, bytes, h_matrix, 0, NULL, NULL);
+	err = clEnqueueWriteBuffer(queue, d_a, CL_TRUE, 0, bytes, h_a, 0, NULL, NULL);
+	err |= clEnqueueWriteBuffer(queue, d_b, CL_TRUE, 0, bytes, h_b, 0, NULL, NULL);
 
+
+	size_t l = max_work_item_sizes[0];
+	//size_t g = 2048;
 	// Set the arguments to our compute kernel
-	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_X);
-	err = clSetKernelArg(kernel, 1, sizeof(unsigned int), &matrixSizeX);
-	err = clSetKernelArg(kernel, 2, sizeof(unsigned int), &matrixSizeY);
+	err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &d_a);
+	err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &d_b);
+	err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &d_c);
+	err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &vectorSize);
 
 	// Execute the kernel over the entire range of the data set  
-	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &globalSize, &localSize, 0, NULL, NULL);
+	err = clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &n, &l, 0, NULL, &event);
 
 	// Wait for the command queue to get serviced before reading back results
 	clFinish(queue);
 
 	// Read the results from the device
-	clEnqueueReadBuffer(queue, d_X, CL_TRUE, 0, bytes, h_matrix, 0, NULL, NULL);
-	if (err == CL_SUCCESS) 
-		printMatrix(h_matrix, matrixSizeX, matrixSizeY);
+	clEnqueueReadBuffer(queue, d_c, CL_TRUE, 0, bytes, h_c, 0, NULL, NULL);
 
+	for (int i = 0; i < vectorSize;i++) {
+		printf("%d\n", h_c[i]);
+	}
 	// release OpenCL resources
 	clReleaseProgram(program);
 	clReleaseKernel(kernel);
 	clReleaseCommandQueue(queue);
 	clReleaseContext(context);
 
-	free(h_matrix);
+	free(h_a);
+	free(h_b);
+	free(h_c);
 }
 
 char* readSourceFile(const char* filename)
@@ -147,19 +171,6 @@ char* readSourceFile(const char* filename)
 	fclose(fp);
 	source[size] = 0;
 	return source;
-}
-
-void printMatrix(const type * matrix, const size_t& n, const size_t& m)
-{
-	cout << endl;
-	for (size_t i = 0; i < n * m; i++)
-	{
-		cout << matrix[i] << " ";
-		if (i % n == n - 1 && i > 0)
-			cout << endl;
-	}
-
-	cout << endl;
 }
 
 
